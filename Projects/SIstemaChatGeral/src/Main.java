b0f412c32868def5944dbff34a18f78d02fd7cb0
gabriel <gabrielpanovich@gmail.com>
2026-05-15 16:00:30 -0300

    Main para teste

--- Projects/SIstemaChatGeral/src/Main.java
+++ Projects/SIstemaChatGeral/src/Main.java
@@ -1,2 +1,29 @@
+import colleagues.BaseColleague;
+import colleagues.User;
+import mediator.Mediator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        var user1 = new User("Banana");
+        var user2 = new User("Arlindo");
+        var user3 = new User("Ubuntu");
+        var user4 = new User("Elon Musk");
+
+        List<BaseColleague> userList = new ArrayList<>();
+        userList.add(user1);
+        userList.add(user2);
+        userList.add(user3);
+        userList.add(user4);
+
+        var mediator = new Mediator(userList);
+
+        user1.sendMessage("Fala gurizada");
+        user3.sendMessage("Opa meus queridos");
+        user4.sendMessage("AOBA");
+        user1.sendMessage("Tirei 0 em SO galeris");
+        user3.sendMessage("OTARIO, tirei 2");
+    }
 }
